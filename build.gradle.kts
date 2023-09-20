28de7285e82bf81aeac1834c0b81386e6ae1c99b
sksamuel <sam@sksamuel.com>
2023-09-20 14:27:15 -0500

    Set jdk to 11

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,11 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
    kotlinOptions.jvmTarget = "11"
 }
 
+java {
+   sourceCompatibility = JavaVersion.VERSION_11
+   targetCompatibility = JavaVersion.VERSION_11
+}
+
 repositories {
    mavenLocal()
    mavenCentral()
